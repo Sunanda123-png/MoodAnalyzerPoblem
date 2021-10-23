5e4b3e1c759c5248abbe7ade3c8e358201f0e0d0
Sunanda123-png <sunanda02@gmail.com>
2021-10-23 23:39:51 +0530

    Add:InformUserEnteredInvalidMood

--- MoodAnalysisTest/com/bridgelabz/MoodAnalysisTsest.java
+++ MoodAnalysisTest/com/bridgelabz/MoodAnalysisTsest.java
@@ -6,8 +6,12 @@ import org.junit.jupiter.api.Assertions;
 public class MoodAnalysisTsest {
     @Test
     void givenMessage_WhenSad_ShouldReturnSad() throws MoodAnalysisException {
-        MoodAnalysis moodAnalyser = new MoodAnalysis();
-        String mood = moodAnalyser.analyseMood("Empty Mood");
-        Assertions.assertEquals("HAPPY", mood);
+        try{
+            MoodAnalysis moodAnalyser = new MoodAnalysis();
+            String mood = moodAnalyser.analyseMood(null);
+            Assertions.assertEquals("HAPPY", mood);
+        }catch (Exception e){
+
+        }
     }
 }
